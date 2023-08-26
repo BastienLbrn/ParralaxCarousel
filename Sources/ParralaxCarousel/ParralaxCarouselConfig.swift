6d0a175e115526fcb67cb421c1a5637b721c4e92
bastien.lebrun <bastien.lebrun@chanel.com>
2023-08-26 19:28:23 +0200

    Initial Commit

---
+++ Sources/ParralaxCarousel/ParralaxCarouselConfig.swift
@@ -0,0 +1,8 @@
+//
+//  ParralaxCarouselConfig.swift
+//  
+//
+//  Created by Bastien Lebrun on 26/08/2023.
+//
+
+import Foundation
---
+++ Sources/ParralaxCarousel/ParralaxContent.swift
@@ -0,0 +1,8 @@
+//
+//  ParralaxContent.swift
+//  
+//
+//  Created by Bastien Lebrun on 26/08/2023.
+//
+
+import Foundation
